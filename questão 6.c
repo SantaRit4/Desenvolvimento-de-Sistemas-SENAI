539b5ad89a4487ee368842be76402e0bf3d6d1cb
Matheus Santa Rita <142445818+SantaRit4@users.noreply.github.com>
2023-11-07 20:11:05 -0300

    Exercicios praticados no Senai

---
+++ questão 6.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define PI 3.14159265359
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	float esferaDia, Raio, Volume;
+	printf("diga o valor do diâmetro da esfera");
+	scanf("%f", &esferaDia);
+	
+	 Raio= esferaDia/2;	
+	 Volume= (1.3333333*PI)*(Raio*Raio*Raio);
+	 
+	 printf("O volume da esfera é  %5f ", Volume);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+	
+}
